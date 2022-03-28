3f19da598649e6b8fd14e63328c9ec071c161fcd
Hrithik <74535054+HRX101@users.noreply.github.com>
2022-03-28 12:35:30 +0530

    Update app.py

--- app.py
+++ app.py
@@ -3,19 +3,21 @@ import cv2
 st.title("hi ")
 frame_window=st.image([])
 run=st.checkbox("run")
-cap=cv2.VideoCapture(cv2.CAP_V4L2)
+
 camera_backends = cv2.videoio_registry.getCameraBackends()
 print(camera_backends)
 print([
     cv2.videoio_registry.getBackendName(apipref)
     for apipref in camera_backends
 ])
-if cap.isOpened():
+def do():
+    cap=cv2.VideoCapture(cv2.CAP_V4L2)
     while run:
         #st.write("hii")
         ret,frame=cap.read()
         frame=cv2.flip(frame,1)
         frame_window.image(frame)
 
-else:
-    st.write("stopped")
+    else:
+        st.write("stopped")
+do()
