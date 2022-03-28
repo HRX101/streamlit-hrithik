76305075f950862b46d3e7d63bf3e3de794f78d5
Hrithik <74535054+HRX101@users.noreply.github.com>
2022-03-28 12:21:20 +0530

    Update app.py

--- app.py
+++ app.py
@@ -3,7 +3,7 @@ import cv2
 st.title("hi ")
 frame_window=st.image([])
 run=st.checkbox("run")
-cap=cv2.VideoCapture(-1)
+cap=cv2.VideoCapture(cv2.CAP_V4L2)
 camera_backends = cv2.videoio_registry.getCameraBackends()
 print(camera_backends)
 print([
