ac024fd696aea74d2f9f14be82d5ab1c3fb83598
Hrithik <74535054+HRX101@users.noreply.github.com>
2022-04-03 13:50:30 +0530

    Update app.py

--- app.py
+++ app.py
@@ -43,8 +43,10 @@ s12=1
 class VideoTransformer(VideoTransformerBase):
     def transform(self, frame):
         img= frame.to_ndarray(format="bgr24")
-        img_gray=cv2.cvtColor(img,cv2.COLOR_BGR2GRAY)
+        p+=1
         img=cv2.flip(img,1)
+        img_gray=cv2.cvtColor(img,cv2.COLOR_BGR2GRAY)
+        
         face=face_detect.detectMultiScale(img_gray,1.4)
         for (x,y,w,h) in face:
             cv2.rectangle(img,(x,y),(x+w,y+h),(255,0,0),2)
